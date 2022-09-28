806884af52a3699fc932ac37337da11b39aeb27e
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2022-09-28 21:03:16 +0200

    add the backend of the submitted link

---
+++ server/controllers/link.js
@@ -0,0 +1,43 @@
+const Link = require('../models/link');
+const slugify = require('slugify');
+
+exports.create = (req, res) => {
+    const { title, url, categories, type, medium } = req.body;
+    // console.table({ title, url, categories, type, medium });
+    const slug = url;
+    let link = new Link({ title, url, categories, type, medium, slug });
+    // posted by user
+    link.postedBy = req.user._id;
+    // save link
+    link.save((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Link already exist'
+            });
+        }
+        res.json(data);
+    });
+};
+
+exports.list = (req, res) => {
+    Link.find({}).exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Could not list links'
+            });
+        }
+        res.json(data);
+    });
+};
+
+exports.read = (req, res) => {
+    //
+};
+
+exports.update = (req, res) => {
+    //
+};
+
+exports.remove = (req, res) => {
+    //
+};
